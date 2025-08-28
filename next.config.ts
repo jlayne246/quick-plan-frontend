14e77f94b7b9a57dc0d023a152ce8101f2dc1315
Joshua Layne <joshualayne2684@gmail.com>
2025-08-27 23:34:37 -0400

    Change to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    // Tell Next.js that the source code is inside src/
-    pageExtensions: ["ts", "tsx", "js", "jsx"],
-    reactStrictMode: true
 };
 
 export default nextConfig;
