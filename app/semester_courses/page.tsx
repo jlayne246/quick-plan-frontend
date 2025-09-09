d6c90de2493a26a682ef7248c9ed8279da1c9727
Joshua Layne <joshualayne2684@gmail.com>
2025-09-09 02:08:37 -0400

    Added potential fix

--- app/semester_courses/page.tsx
+++ app/semester_courses/page.tsx
@@ -1,11 +1,14 @@
 // page.tsx (server)
 
 import SemesterCourses from "./SemesterCourses";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div>
-      <SemesterCourses /> {/* client component */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <SemesterCourses /> {/* client component */}
+      </Suspense>
     </div>
   );
 }
